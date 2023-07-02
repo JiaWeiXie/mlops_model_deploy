6d1fe87c2f39bcc47522631101b27b8389234952
jasonxie <x5758x@gmail.com>
2023-07-03 02:50:12 +0800

    add ci deploy model

--- import_model.py
+++ import_model.py
@@ -10,7 +10,11 @@ MLFLOW_MODEL_URI = os.getenv("MLFLOW_MODEL_URI")
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URL)
 
 logi_clf_model = mlflow.sklearn.load_model(MLFLOW_MODEL_URI)
+
+print(f"Loaded sklearn model: {logi_clf_model}.")
 model_tmp_path = f"/tmp/{logi_clf_model}"
 mlflow.sklearn.save_model(logi_clf_model, model_tmp_path)
-bentoml.mlflow.import_model("logi_clf_model", model_uri=model_tmp_path)
+print(f"Saved sklearn model to local.")
+bentoml_model = bentoml.mlflow.import_model("logi_clf_model", model_uri=model_tmp_path)
+print(f"Import sklearn model to bentoml model: {bentoml_model}.")
 # saved_model = bentoml.sklearn.save_model("iris_clf", clf)
