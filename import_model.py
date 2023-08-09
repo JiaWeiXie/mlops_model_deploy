70036b7e4358e9d5cb267a6b530680aab78f8738
jasonxie <x5758x@gmail.com>
2023-08-09 10:54:45 +0800

    add comment

--- import_model.py
+++ import_model.py
@@ -1,20 +1,22 @@
 import os
-
 import bentoml
 import mlflow
 
 
+# 從環境變數取得 MLflow 相關設定參數
 MLFLOW_TRACKING_URL = os.getenv("MLFLOW_TRACKING_URL")
 MLFLOW_MODEL_URI = os.getenv("MLFLOW_MODEL_URI")
-
+# 設定 MLflow tracking server 網址
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URL)
-
+# 載入遠端儲存的模型
 logi_clf_model = mlflow.sklearn.load_model(MLFLOW_MODEL_URI)
-
 print(f"Loaded sklearn model: {logi_clf_model}.")
+# 宣告模型暫存路徑
 model_tmp_path = f"/tmp/{logi_clf_model}"
+# 儲存模型到暫存路徑
 mlflow.sklearn.save_model(logi_clf_model, model_tmp_path)
 print(f"Saved sklearn model to local.")
+# 從暫存路徑匯入模型到 BentoML 工作目錄
 bentoml_model = bentoml.mlflow.import_model("logi_clf_model", model_uri=model_tmp_path)
 print(f"Import sklearn model to bentoml model: {bentoml_model}.")
 # saved_model = bentoml.sklearn.save_model("iris_clf", clf)
--- service.py
+++ service.py
@@ -1,14 +1,21 @@
 import numpy as np
-
 import bentoml
 from bentoml.io import NumpyNdarray
+
 
 logi_clf_runner = bentoml.mlflow.get("logi_clf_model:latest").to_runner()
 
 svc = bentoml.Service("logi_clf", runners=[logi_clf_runner])
 
-arr = [17.99, 10.38, 122.8, 1001.0, 0.1184, 0.2776, 0.3001, 0.1471, 0.2419, 0.07871, 1.095, 0.9053, 8.589, 153.4, 0.006399, 0.04904, 0.05373, 0.01587, 0.03003, 0.006193, 25.38, 17.33, 184.6, 2019.0, 0.1622, 0.6656, 0.7119, 0.2654, 0.4601, 0.1189]
-input_spec = NumpyNdarray.from_sample(np.array([arr]))
+sample_data = [
+    17.99, 10.38, 122.8, 1001.0, 0.1184,
+    0.2776, 0.3001, 0.1471, 0.2419, 0.07871,
+    1.095, 0.9053, 8.589, 153.4, 0.006399,
+    0.04904, 0.05373, 0.01587, 0.03003, 0.006193,
+    25.38, 17.33, 184.6, 2019.0, 0.1622,
+    0.6656, 0.7119, 0.2654, 0.4601, 0.1189,
+]
+input_spec = NumpyNdarray.from_sample(np.array([sample_data]))
 
 @svc.api(
     input=input_spec,
